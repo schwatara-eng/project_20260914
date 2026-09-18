abe8d14d5902d84ab87ca925b706c5c44012a0e2
schwatara-eng <schwatara@gmail.com>
2026-09-18 15:05:18 +0900

    1

---
+++ javascript 자습 0918.js
@@ -0,0 +1,21 @@
+function fgugu() {
+    // 구구단
+    for (let dan = 2; dan < 10; dan++) { //바깥쪽반복(단)
+        console.log(dan + "단")
+        for (let i = 1; i < 10; i++) console.log(dan + "*" + i + "=" + dan * i)
+        console.log("===")
+    }
+}
+
+function fgugu2(dan) {
+    // 구구단
+    console.log(dan + "단")
+    for (let i = 1; i < 10; i++) console.log(dan + "*" + i + "=" + dan * i)
+    console.log("===")
+    return "성공"
+}
+
+
+
+
+
